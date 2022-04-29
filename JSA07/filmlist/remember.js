926f8620efcc107cf2fbb966b98c3d0af51bc971
thich <97292950+2k8hahadaylatao@users.noreply.github.com>
2022-04-29 19:40:00 +0700

    16%
     16%% .-.

--- JSA07/filmlist/remember.js
+++
@@ -1,8 +0,0 @@
-var data = [
-    {
-        name:"ADMIN",
-        pass:"aaaaaa"
-    }
-    ,
-
-]
--- JSA07/jeb/app.js
+++
@@ -1,8 +0,0 @@
-const dark= localStorage.getItem("dark");
-if(dark==null ) {dark=false}
-const input = document.querySelector("[name=dark]");
-input.checked = darkmode;
-input.onchange = function()
-{
-    localStorage.setItem("dark",input.checked);
-}
